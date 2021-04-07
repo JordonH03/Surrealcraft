4d9dedc2396b1f7f8eb97575fbae0cc84c6b8322
JordonH03 <57729746+JordonH03@users.noreply.github.com>
2021-04-06 23:07:36 -0500

    Added useful folders and Init classes (item, block, ore gen)

--- src/main/java/com/mustardd/surrealcraft/Surrealcraft.java
+++ src/main/java/com/mustardd/surrealcraft/Surrealcraft.java
@@ -1,43 +1,43 @@
 package com.mustardd.surrealcraft;
 
-import java.util.stream.Collectors;
-
 import com.mustardd.surrealcraft.init.BlockInit;
 import com.mustardd.surrealcraft.init.ItemInit;
-import net.minecraftforge.eventbus.api.IEventBus;
-import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.LogManager;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.InterModComms;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.event.RegistryEvent;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraft.block.Blocks;
 import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.InterModComms;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
+import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
-// Mod ID. Always lowercase. Use anytime code requires modID
 @Mod("surrealcraft")
-
 public class Surrealcraft
 {
     // Directly reference a log4j logger.
-    public static final Logger LOGGER = LogManager.getLogger();
+    private static final Logger LOGGER = LogManager.getLogger();
+    // Initialize string constant for mod ID
+    public  static final String MOD_ID = "surrealcraft";
 
-    // Initialize a modID constant
-    public static final String MOD_ID = "surrealcraft";
     public Surrealcraft() {
-        // Initialize a variable for the event bus for ease of use
+        // Initialize a variable for the bus for ease of use
         IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
 
         // Register the setup method for modloading
         bus.addListener(this::setup);
+        // Register the doClientStuff method for modloading
+        bus.addListener(this::doClientStuff);
         // Register the ITEMS
         ItemInit.ITEMS.register(bus);
         // Register the BLOCKS
@@ -47,6 +47,13 @@ public class Surrealcraft
         MinecraftForge.EVENT_BUS.register(this);
     }
 
-    private void setup(final FMLCommonSetupEvent event) {}
+    private void setup(final FMLCommonSetupEvent event)
+    {}
+
+    private void doClientStuff(final FMLClientSetupEvent event) {}
+
+    // You can use SubscribeEvent and let the Event Bus discover methods to call
+    @SubscribeEvent
+    public void onServerStarting(FMLServerStartingEvent event) {}
 
 }
