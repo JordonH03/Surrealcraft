cf70239f9cebf4eef2550089cc046acea606d6d7
JordonH03 <57729746+JordonH03@users.noreply.github.com>
2021-04-29 20:19:31 -0500

    Added ruby wand functionality

--- src/main/java/com/mustardd/surrealcraft/advanced_items/CitrineWand.java
+++ src/main/java/com/mustardd/surrealcraft/advanced_items/CitrineWand.java
@@ -1,6 +1,7 @@
 package com.mustardd.surrealcraft.advanced_items;
 
 import com.mustardd.surrealcraft.init.ItemInit;
+import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -10,10 +11,14 @@ import net.minecraft.potion.Effects;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.ActionResultType;
 import net.minecraft.util.Hand;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.world.World;
 import net.minecraftforge.common.Tags;
 
+import javax.annotation.Nullable;
 import javax.swing.*;
+import java.util.List;
 
 public class CitrineWand extends Item {
     // Constructor for item
@@ -34,6 +39,15 @@ public class CitrineWand extends Item {
         }
 
         return super.use(world, player, handIn);
+    }
 
+    // Adds hover text showing item usages and maximum charges
+    @Override
+    public void appendHoverText(ItemStack itemStack, @Nullable World world, List<ITextComponent> iTextComponents, ITooltipFlag iTooltipFlag) {
+        iTextComponents.add(new StringTextComponent(" "));
+        iTextComponents.add(new StringTextComponent("\u00A77" + "When in main hand:"));
+        iTextComponents.add(new StringTextComponent("\u00A72" + "Right-click to heal 1 heart"));
+        iTextComponents.add(new StringTextComponent("\u00A72" + "10 charges"));
+        super.appendHoverText(itemStack, world, iTextComponents, iTooltipFlag);
     }
 }
--- src/main/java/com/mustardd/surrealcraft/advanced_items/RubyWand.java
+++ src/main/java/com/mustardd/surrealcraft/advanced_items/RubyWand.java
@@ -1,7 +1,10 @@
 package com.mustardd.surrealcraft.advanced_items;
 
+import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.monster.GhastEntity;
 import net.minecraft.entity.passive.ChickenEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.projectile.FireballEntity;
@@ -13,8 +16,13 @@ import net.minecraft.util.Direction;
 import net.minecraft.util.Hand;
 import net.minecraft.util.math.*;
 import net.minecraft.util.math.vector.Vector3d;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.world.World;
 import net.minecraft.world.storage.IWorldInfo;
+
+import javax.annotation.Nullable;
+import java.util.List;
 
 public class RubyWand extends Item {
     // Constructor for item
@@ -26,9 +34,21 @@ public class RubyWand extends Item {
     @Override
     public ActionResult<ItemStack> use(World world, PlayerEntity player, Hand hand) {
         ItemStack stack = player.getItemInHand(hand);
-        Vector3d lookpos = player.getEyePosition(1.0F); // direction of where player looks
-        // player.getProjectile();
-        //getProjectile(new FireballEntity(world, 0.0F, 0.0F, 0.0F, 0.0F, 0.0F, 0.0F))
+        player.getCooldowns().addCooldown(this, 40); // 2 second cooldown
+
+        // Code from Minecraft GhastEntity
+        Vector3d vector3d = player.getViewVector(1.0F);
+        // Sets fireball direction to player look direction
+        // Simplified version of Ghast aiming
+        double d2 = vector3d.x; // double d2 = livingentity.getX() - (this.ghast.getX() + vector3d.x * 4.0D);
+        double d3 = vector3d.y; // double d3 = livingentity.getY(0.5D) - (0.5D + this.ghast.getY(0.5D));
+        double d4 = vector3d.z; // double d4 = livingentity.getZ() - (this.ghast.getZ() + vector3d.z * 4.0D);
+
+        FireballEntity fireballentity = new FireballEntity(world, player, d2, d3, d4); // creates fireball entity
+        fireballentity.explosionPower = 2;
+        fireballentity.setPos(player.getX() + vector3d.x * 4.0D, player.getY(0.5D) + 0.5D, fireballentity.getZ() + vector3d.z * 4.0D); // Set fireball position
+        world.addFreshEntity(fireballentity); // Summons fireball into world
+
 
         stack.setDamageValue(getDamage(stack) + 5); // Adds 5 point of damage to item
         if(stack.getDamageValue() > 50) {
@@ -38,19 +58,13 @@ public class RubyWand extends Item {
 
     }
 
-    protected static BlockRayTraceResult getPlayerPOVHitResult(World world, PlayerEntity player, RayTraceContext.FluidMode fluidMode) {
-        double range = player.getAttribute(net.minecraftforge.common.ForgeMod.REACH_DISTANCE.get()).getValue();
-
-        float f = player.xRot;
-        float f1 = player.yRot;
-        Vector3d vector3d = player.getEyePosition(1.0F);
-        float f2 = MathHelper.cos(-f1 * ((float)Math.PI / 180F) - (float)Math.PI);
-        float f3 = MathHelper.sin(-f1 * ((float)Math.PI / 180F) - (float)Math.PI);
-        float f4 = -MathHelper.cos(-f * ((float)Math.PI / 180F));
-        float f5 = MathHelper.sin(-f * ((float)Math.PI / 180F));
-        float f6 = f3 * f4;
-        float f7 = f2 * f4;
-        Vector3d vector3d1 = vector3d.add((double)f6 * range, (double)f5 * range, (double)f7 * range);
-        return world.clip(new RayTraceContext(vector3d, vector3d1, RayTraceContext.BlockMode.OUTLINE, fluidMode, player));
+    // Adds hover text showing item usages and maximum charges
+    @Override
+    public void appendHoverText(ItemStack itemStack, @Nullable World world, List<ITextComponent> iTextComponents, ITooltipFlag iTooltipFlag) {
+        iTextComponents.add(new StringTextComponent(" "));
+        iTextComponents.add(new StringTextComponent("\u00A77" + "When in main hand:"));
+        iTextComponents.add(new StringTextComponent("\u00A72" + "Right-click to launch fireball"));
+        iTextComponents.add(new StringTextComponent("\u00A72" + "10 charges"));
+        super.appendHoverText(itemStack, world, iTextComponents, iTooltipFlag);
     }
 }
